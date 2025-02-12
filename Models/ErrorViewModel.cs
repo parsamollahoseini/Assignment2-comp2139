a498f2ab350e4d241dbc895555213714b955789e
parsa molahosseini <parsamollahoseini7@gmail.com>
2025-02-12 13:57:16 -0500

    Initial commit - COMP 2139 Assignment 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SmartInventory.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
